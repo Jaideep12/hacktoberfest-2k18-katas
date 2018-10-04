5e968ef678d08221270686a2a42a1ac4284148b6
Jaideep12 <jaideeps85@gmail.com>
2018-10-04 10:39:11 +0530

    Add files via upload

--- src/isDivisibleByThree.js
+++ src/isDivisibleByThree.js
@@ -1,9 +1,18 @@
-function checkDiv(var n)
+function checkDiv(n)
 {
-	if(n%3==0)
+	var sum=0;
+	var last=0;
+	while(n!=0)
 	{
-		return true;
+		last=n%10;
+		sum+=last;
+		n=n/10;
 	}
+	//var res=Math.round(sum);
+	var res=parseInt(sum);
+	console.log(res);
+	if(res%3==0)
+		console.log("Divisible");
 	else
-		return false;
+		console.log("Not divisible");
 }
